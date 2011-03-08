5b144837679ab7d423e872bc0ee9d1ab88547e6e
Joseph Huang <j2five@yahoo.com>
2011-03-08 18:11:52 -0500

    bullet hit state

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ server = http.createServer(function(req, res){
   switch (path){
     case '/':
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('<h1><a href="index.html">Play</a></h1>');
+      res.write('<a href="test.html">play</a>');
       res.end();
       break;
      
@@ -58,7 +58,7 @@ send404 = function(res){
   res.end();
 };
 
-server.listen(80);
+server.listen(8080, '127.0.0.1');
 
 var io = io.listen(server)
   , buffer = []
@@ -72,13 +72,13 @@ io.on('connection', function(client){
   
   client.on('message', function(message){
     var msg = { message: [client.sessionId, message] };
-    console.log(msg);
+    //console.log(msg);
     switch(message.type) {
 	  case 'heartbeat':
 	  	client.broadcast({type: 'new_player_connected', players:players});
 	  	break;
       case 'player_connected':
-	  console.log("player connected "+message.id);
+	 // console.log("player connected "+message.id);
 	  	if(!players[message.id])players[message.id]={id:message.id,x:message.x,z:message.z,rotation:message.rotation};
 		clients_id[client.sessionId] = message.id;
 		client.broadcast({type: 'new_player_connected', players:players});
@@ -88,13 +88,16 @@ io.on('connection', function(client){
 		break;
       case 'player_hit':
 	  	if(players[message.tid]&&players[message.id]){
-				if(Math.abs(players[message.tid].x-message.x)<10&&Math.abs(players[message.tid].z-message.z)<10){
+		  	//console.log("player hit "+players[message.tid].x+" x:"+message.x);
+			//console.log("player hit "+players[message.tid].z+" x:"+message.z)
+			if(Math.abs(players[message.tid].x-message.x)<10&&Math.abs(players[message.tid].z-message.z)<10){
+					//console.log("player hit hit hit");
 					client.broadcast({type:'player_hit',id:message.id,tid:message.tid});
 				}
 		}
 		break;
 	  case 'player_moved':
-		  console.log("player moved "+message.id);
+		  //console.log("player moved "+message.id);
 	
 			if(players[message.id]){
 				players[message.id].x=message.x;
